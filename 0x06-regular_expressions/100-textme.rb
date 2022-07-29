37cd3d7dc8a3080c15cbd29c6485d43ed93bd732
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-07-29 18:33:41 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-from = ARGV[0].scan(/\[from:[0-9]*\]/)
-to = ARGV[0].scan(/\[to:[0-9]*\]/)
+from = ARGV[0].scan(/\[from:(.*)\]/)
+to = ARGV[0].scan(/\[to:(.*)\]/)
 flags = ARGV[0].scan(/\[flags:[0-9-:]*\]/)
 puts [from, to, flags].join(',')
